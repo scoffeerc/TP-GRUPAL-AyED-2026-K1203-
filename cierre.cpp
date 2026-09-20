52d0dca22b5f5d0e56206b0e0f746ea7be1a25c4
scoffeerc <sabrichamis@gmail.com>
2026-09-20 12:12:16 -0300

    cierre: limite de registros con aviso y chequeo al crear el semanal, agregado el control minimo de dias, no cambia mucho

--- cierre.cpp
+++ cierre.cpp
@@ -11,22 +11,27 @@ struct Comanda {
     float comision;
 };
 
-const int MAX_REGISTROS = 5000;
+const int MAX_REGISTROS = 20000;
+
+// Global: no ocupa la pila, asi que puede ser grande.
+Comanda todos[MAX_REGISTROS];
 
 int main (){
-    Comanda todos [MAX_REGISTROS];
-    int cantTotal =0;
+    int cantTotal = 0;
     int cantDias;
+    bool excedido = false;
+
     cout << "Cuantos dias tiene la semana a cerrar? ";
     cin >> cantDias;
 
     // 1) leo cada día y junto todo en un array
-    for (int i =0; i< cantDias; i++){
-        char fecha [11];
+    for (int i = 0; i < cantDias; i++){
+        char fecha[50];
         cout << "Fecha del dia " << (i + 1) << " (dd-mm-aaaa): ";
+        cin.width(50);
         cin >> fecha;
 
-        char nombreArchivo[30];
+        char nombreArchivo[100];
         strcpy(nombreArchivo, "comandas_");
 
         int len = 0;
@@ -39,23 +44,33 @@ int main (){
 
         FILE* fDia = fopen(nombreArchivo, "rb");
         if (fDia == NULL){
-        cout << "  no hay planilla para ese dia, se saltea" << endl;
+            cout << "  no hay planilla para ese dia, se saltea" << endl;
         }
         else{
-        Comanda c;
-        int LeidosDeEsteDia=0;
-        while (fread(&c, sizeof(Comanda), 1, fDia) ==1){
-            todos [cantTotal] = c;
-            cantTotal++;
-            LeidosDeEsteDia++;
-        }
-        fclose (fDia);
-        cout << "  " << LeidosDeEsteDia << " ventas cargadas de " << nombreArchivo << endl;
+            Comanda c;
+            int LeidosDeEsteDia = 0;
+            while (!excedido && fread(&c, sizeof(Comanda), 1, fDia) == 1){
+                if (cantTotal >= MAX_REGISTROS) {
+                    excedido = true;
+                } else {
+                    todos[cantTotal] = c;
+                    cantTotal++;
+                    LeidosDeEsteDia++;
+                }
+            }
+            fclose(fDia);
+            cout << "  " << LeidosDeEsteDia << " ventas cargadas de " << nombreArchivo << endl;
         }
     }
 
-// 2) Ordeno todo el array por id_Mozo
-for (int i=0; i< cantTotal - 1; i++) {
+    if (excedido) {
+        cout << "Error: la semana tiene mas de " << MAX_REGISTROS
+             << " ventas. No se genero la planilla semanal." << endl;
+        return 1;
+    }
+
+    // 2) Ordeno todo el array por id_Mozo
+    for (int i = 0; i < cantTotal - 1; i++) {
         int menor = i;
         for (int j = i + 1; j < cantTotal; j++) {
             if (todos[j].idMozo < todos[menor].idMozo) {
@@ -69,39 +84,45 @@ for (int i=0; i< cantTotal - 1; i++) {
         }
     }
 
-// 3) pido el nombre del archivo semanal y lo escribo
-char semana[10], mes[3];
-cout << "Numero de semana (ej 1): ";
-cin >> semana;
-cout << "Mes (ej 06): ";
-cin >> mes;
+    // 3) pido el nombre del archivo semanal y lo escribo
+    char semana[50], mes[50];
+    cout << "Numero de semana (ej 1): ";
+    cin.width(50);
+    cin >> semana;
+    cout << "Mes (ej 06): ";
+    cin.width(50);
+    cin >> mes;
 
-char nombreSemanal [40];
-strcpy(nombreSemanal, "comandas_semana_s");
+    char nombreSemanal[200];
+    strcpy(nombreSemanal, "comandas_semana_s");
 
-int lenS = 0;
-while (nombreSemanal[lenS] != '\0') lenS++;
-strcpy(nombreSemanal + lenS, semana);
+    int lenS = 0;
+    while (nombreSemanal[lenS] != '\0') lenS++;
+    strcpy(nombreSemanal + lenS, semana);
 
-lenS = 0;
-while (nombreSemanal[lenS] != '\0') lenS++;
-strcpy(nombreSemanal + lenS, "-");
+    lenS = 0;
+    while (nombreSemanal[lenS] != '\0') lenS++;
+    strcpy(nombreSemanal + lenS, "-");
 
-lenS = 0;
-while (nombreSemanal[lenS] != '\0') lenS++;
-strcpy(nombreSemanal + lenS, mes);
+    lenS = 0;
+    while (nombreSemanal[lenS] != '\0') lenS++;
+    strcpy(nombreSemanal + lenS, mes);
 
-lenS = 0;
-while (nombreSemanal[lenS] != '\0') lenS++;
-strcpy(nombreSemanal + lenS, ".dat");
+    lenS = 0;
+    while (nombreSemanal[lenS] != '\0') lenS++;
+    strcpy(nombreSemanal + lenS, ".dat");
 
-FILE* fSemana = fopen(nombreSemanal, "wb");
-for (int i=0; i < cantTotal; i++) {
-    fwrite(&todos[i], sizeof(Comanda), 1, fSemana);
-}
-fclose(fSemana);
+    FILE* fSemana = fopen(nombreSemanal, "wb");
+    if (fSemana == NULL) {
+        cout << "Error: no se pudo crear " << nombreSemanal << endl;
+        return 1;
+    }
+    for (int i = 0; i < cantTotal; i++) {
+        fwrite(&todos[i], sizeof(Comanda), 1, fSemana);
+    }
+    fclose(fSemana);
 
-cout << endl << "Listo. Se genero " << nombreSemanal<< " con " << cantTotal << " ventas en total." << endl;
+    cout << endl << "Listo. Se genero " << nombreSemanal << " con " << cantTotal << " ventas en total." << endl;
 
-return 0;
+    return 0;
 }
